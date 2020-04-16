0166acb6ac5d0f735adc00febd1e4a25963fddc2
Rifky Fauzi <rifkyfauzi9@gmail.com>
2020-04-17 00:43:45 +0700

    Update runge_kutta_solver.py

--- runge_kutta_solver.py
+++ runge_kutta_solver.py
@@ -1,4 +1,3 @@
-#kode hanya angin
 
 import math as ma
 import matplotlib.pyplot as plt
@@ -6,35 +5,44 @@ import numpy as np
 from matplotlib import rc
 
 
-amax = init1;
-def f(x, y, Us, mu, sig) : return [y,   ( 1/F - ((1 - Us)**2)/(x**3) )*y/mu - y**2/x - (x-1)*(x**2 + (1-Us**2)*x + (1-Us)**2)/(mu*sig*x**3)  ];
 
+def SIR(x, param) : 
+    S = x[0]
+    I = x[1]
+    R = x[2]
+    beta = param[0]
+    gamma = param[1]
+    
+    N = S + I + R
 
-ls = [ '-', '--', '-.', ':', 'o', 'v', '^', '<', '>', '1', '2', '3', '4','s', 'p', '*', 'h', 'H','+','x','D','d','|', '_']; 
+    dS = -beta*S*I/N
+    dI = beta*S*I/N - gamma*I
+    dR = gamma*I
+    
+    return np.array([dS, dI, dR])
 
+def RK4solver(func, param, tspan, init):
+    dim = len(init)
+    ntime = len(tspan)
+    dz = tspan[1] - tspan[0]
+    y = np.zeros((ntime, dim))
+    y[0,:] = init
+    
+    n = 0
+    while (n < ntime - 1):
+        k1             = func(y[n,:]                , param )
+        k2             = func(y[n,:] + 0.5 * dz * k1, param )
+        k3             = func(y[n,:] + 0.5 * dz * k2, param )
+        k4             = func(y[n,:] +       dz * k3, param )
+        y[n + 1,:]     = y[n,:] + dz / 6 * ( k1 + 2 * k2 + 2 * k3 + k4 )
+        n = n + 1;
+    return y
 
-h = [[]]*len(mun);
-w = [[]]*len(mun);
-wmin = [];
-hmin = [];
-for j in range( len(mun)):
-    mu = mun[j]
-    for k in range(50):
-        y1 = [amax]; #h
-        y2 = [0]; #w
-        for n in range(Nt):
-                     k1          = f(y1[n]                    , y2[n]                   , c,  mu, sig );
-                     k2          = f(y1[n] + 0.5 * dz * k1[0] , y2[n] + 0.5 * dz * k1[1] , c,  mu, sig );
-                     k3          = f(y1[n] + 0.5 * dz * k2[0] , y2[n] + 0.5 * dz * k2[1] , c,  mu, sig );
-                     k4          = f(y1[n] +       dz * k3[0] , y2[n] +       dz * k3[1] , c,  mu, sig );
-                     y1.append(y1[n] + dz / 6 * ( k1[0] + 2 * k2[0] + 2 * k3[0] + k4[0] ));
-                     y2.append(y2[n] + dz / 6 * ( k1[1] + 2 * k2[1] + 2 * k3[1] + k4[1] ));
-                     n = n + 1;
-        amax = max(y1);
-        amin = min(y1);
-    wmin.append(min(y2));
-    hmin.append(min(y1));
-    h[j] = y1;
-    w[j] = y2;
-    amax = init1;
-
+#simulation
+R0 = 2.2
+gamma = 1/10
+beta = gamma*R0
+param = [beta, gamma]
+y0 = [N-1, 1, 0]
+tspan = np.linspace(0,20,100)
+y = RK4solver(SIR, param, tspan, y0)
